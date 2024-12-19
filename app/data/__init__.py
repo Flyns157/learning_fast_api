4a3d30c8b20aeaab8f37f09d1f54aa812ab61fb1
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-12-19 20:32:23 +0100

    Refactor Pokémon data handling and API routes for better functionality. Changed data return types, integrated error handling, and added explicit add/pokemon functionality. Improved delete operation in the API.

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -11,22 +11,45 @@ def get_users() -> dict[User]:
 
 
 # ====== Pokemon data ===== #
-def get_data() -> dict[Pokemon]:
+def get_data() -> list[Pokemon]:
     #===== Structure de données : Dictionnaire indexé par pokemon id =====#
     with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "r") as f:
-        return json.load(f)
+        return [Pokemon(**pokemon) for pokemon in json.load(f)]
 
-def get_pokemon(pokemon_id) -> dict:
-    return get_data().get(str(pokemon_id))
+def get_pokemon(pokemon_id) -> Pokemon | None:
+    with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "r") as f:
+        for pokemon in json.load(f):
+            if pokemon["id"] == pokemon_id:
+                return Pokemon(**pokemon)
 
-def put_pokemon(pokemon_id: int | str, pokemon_data: Pokemon) -> None:
+def put_pokemon(pokemon_id: int | str, pokemon_data: Pokemon) -> bool:
+    if pokemon_id != pokemon_data.id:
+        return False
     data = get_data()
-    data[str(pokemon_id)] = pokemon_data
-    with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "w") as f:
-        json.dump(data, f)
+    try:
+        data[data.index(pokemon_data.model_dump())] = pokemon_data
+        with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "w") as f:
+            json.dump([pokemon.model_dump() for pokemon in data], f)
+        return True
+    except ValueError:
+        return False
 
-def del_pokemon(pokemon_id: int | str) -> None:
+def add_pokemon(pokemon_data: Pokemon) -> bool:
     data = get_data()
-    data.pop(str(pokemon_id))
-    with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "w") as f:
-        json.dump(data, f)
+    if all(pokemon.id != pokemon_data.id for pokemon in data):
+        data.append(pokemon_data)
+        with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "w") as f:
+            json.dump([pokemon.model_dump() for pokemon in data], f)
+        return True
+    return False
+
+def del_pokemon(pokemon_id: int | str) -> Pokemon | None:
+    data = get_data()
+    i = 0
+    while i < len(data):
+        if data[i]["id"] == pokemon_id:
+            tmp = data.pop(i)
+            with open(os.path.join(os.path.dirname(__file__), "pokemons.json"), "w") as f:
+                json.dump([pokemon for pokemon in data if pokemon["id"] != pokemon_id], f)
+            return tmp
+        i += 1
--- app/routers/pokemon.py
+++ app/routers/pokemon.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Path
-from ..data import get_data, get_pokemon, put_pokemon, del_pokemon
+from ..data import add_pokemon, get_pokemon, put_pokemon, del_pokemon
 from ..models.pokemon import Pokemon
 
 router = APIRouter(prefix="/pokemon")
@@ -8,38 +8,32 @@ router = APIRouter(prefix="/pokemon")
 #===========================GET============================
 @router.get("/{id}", response_model=Pokemon)
 async def get_pokemon_by_id(id: int = Path(ge=1)) -> Pokemon:
-    if id not in get_data():
+    pokemon = get_pokemon(id)
+    if not pokemon:
         raise HTTPException(status_code=404, detail="Ce pokemon n'existe pas")
-    
-    return Pokemon(**get_pokemon(id))
+    return pokemon
 
 
 #===========================POST============================
 @router.post("/", response_model=Pokemon)
 async def create_pokemon(pokemon: Pokemon) -> Pokemon:
-    if get_pokemon(pokemon.id):
+    if add_pokemon(pokemon.id, pokemon):
         raise HTTPException(status_code=404, detail=f"Le pokemon {pokemon.id} existe déjà !")
-    
-    put_pokemon(pokemon.id, pokemon.model_dump())
     return pokemon
 
 
 #===========================PUT============================
 @router.put("/{id}", response_model=Pokemon)
 async def update_pokemon(pokemon: Pokemon, id: int = Path(ge=1)) -> Pokemon:
-    if id not in get_data():
+    if put_pokemon(id, pokemon):
         raise HTTPException(status_code=404, detail=f"Le pokemon {id} n'existe pas.")
-    
-    put_pokemon(id, pokemon.model_dump())
     return pokemon
 
 
 #===========================DELETE============================
 @router.delete("/{id}", response_model=Pokemon)
 async def delete_pokemon(id: int = Path(ge=1)) -> Pokemon:
-    if id in get_data():
-        pokemon = Pokemon(**get_pokemon(id))
-        del_pokemon(id)
-        return pokemon
-    
-    raise HTTPException(status_code=404, detail=f"Le pokemon {id} n'existe pas.")
+    pokemon = del_pokemon(id)
+    if pokemon is None:
+        raise HTTPException(status_code=404, detail=f"Le pokemon {id} n'existe pas.")
+    return pokemon
